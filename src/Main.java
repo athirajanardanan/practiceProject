f479c8b8faff09296b88a71acbd834205648ce30
athirajanardanan <athi.j89@gmail.com>
2022-12-21 18:25:57 -0500

    First commit from personB

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello,Iam person A");
-        System.out.println("This is my basic program");
-        System.out.println("Hiii this is my third line");
+System.out.println("IAm personB");
     }
 }
