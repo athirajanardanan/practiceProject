81f58eef426216366745a431b717c7d62f5c7a4a
athirajanardanan <athi.j89@gmail.com>
2022-12-21 17:51:01 -0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello,Iam person A");
         System.out.println("This is my basic program");
     }
 }
--- src/submain.java
+++ src/submain.java
@@ -2,7 +2,8 @@ public class submain {
 
     public static void  main(String[] args)
     {
-        System.out.println("This is my sec class");
+        System.out.println("This is my sec class.And IAm person A");
+        System.out.println("Hiiii there");
 
     }
 }
